f0f17d7587a46aad24d2f9df6dd47f884af4d511
Maria Julia <maju.m.valaski@gmail.com>
2023-06-11 18:28:41 -0300

    calculadora pronta

--- Ex2/Ex2.js
+++ Ex2/Ex2.js
@@ -3,3 +3,19 @@ const campo2 = document.querySelector("#campo2");
 const seletor = document.querySelector("#operacao");
 const botao = document.querySelector("#igual");
 let resposta = document.querySelector("#resposta");
+
+botao.addEventListener("click", calcular);
+
+function calcular(){
+    const valor1 = parseInt(campo1.value);
+    const valor2 = parseInt(campo2.value);
+    const operacao = seletor.value
+    if (operacao =="somar")
+        resposta.innerHTML = valor1 + valor2;
+    if (operacao =="subtrair")
+        resposta.innerHTML = valor1 - valor2;
+    if (operacao =="multiplicar")
+        resposta.innerHTML = valor1 * valor2;
+    if (operacao =="dividir")
+        resposta.innerHTML = valor1 / valor2;                 
+}
