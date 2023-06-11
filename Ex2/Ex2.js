e847d0338d8fc34b9ce12fa61f85c93e92046a47
Maria Julia <maju.m.valaski@gmail.com>
2023-06-11 18:15:43 -0300

    definindo as variaveis com querySelector

---
+++ Ex2/Ex2.js
@@ -0,0 +1,5 @@
+const campo1 = document.querySelector("#campo1");
+const campo2 = document.querySelector("#campo2");
+const seletor = document.querySelector("#operacao");
+const botao = document.querySelector("#igual");
+let resposta = document.querySelector("#resposta");
