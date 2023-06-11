f092223691fbcb3da816e3ef9708d99f8e6c086a
Maria Julia <maju.m.valaski@gmail.com>
2023-06-11 18:07:47 -0300

    so quebra quando clica 10 vezes

--- Ex1.js
+++ Ex1.js
@@ -1,22 +1,28 @@
 let botao = document.querySelector("#botao");
 botao.style.background="blue";
 let estaQuebrado =false;
+let contaCliques =0;
 
 /*outro jeito de escrever a funçao */
 botao.addEventListener("mouseover", e =>{
-    if (estaQuebrado==false)
+    if (estaQuebrado==false) /*para nao trocar depois de ter clicado*/
         botao.style.background="green";
 });
 botao.addEventListener("mouseout", fundoAzul);
 
 
 function fundoAzul(){
-    if (estaQuebrado==false)
+    if (!estaQuebrado) /*para nao trocar depois de ter clicado*/
         botao.style.background="blue";
 }
 
 botao.addEventListener("click", e =>{
-    botao.style.background="red";
-    botao.innerHTML="quebrei";
-    estaQuebrado =true;
+    contaCliques ++;//conta quantos cliques. ContaCliques = ContaCliques+1
+    if (contaCliques>=10){
+        botao.style.background="red";
+        botao.innerHTML="quebrei";
+        estaQuebrado =true;
+    }
+    
+    
 });
